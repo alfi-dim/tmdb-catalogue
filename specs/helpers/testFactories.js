f05444f31132b122bdd8e840126b4db259ff9fd2
Zatfley <alfi.dim1612@gmail.com>
2023-02-19 10:22:22 +0700

    create test driven for search movie

--- specs/helpers/testFactories.js
+++ specs/helpers/testFactories.js
@@ -1,8 +1,10 @@
+import FavoriteMovieIdb from '../../src/scripts/data/favorite-movie-idb';
 import LikeButtonInitiator from '../../src/scripts/utils/like-button-presenter';
 
 const createLikeButtonPresenterWithMovie = async (movie) => {
   await LikeButtonInitiator.init({
     likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    favoriteMovies: FavoriteMovieIdb,
     movie,
   });
 };
---
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -0,0 +1,37 @@
+class FavoriteMovieSearchPresenter {
+  constructor({ favoriteMovies }) {
+    this._listenToSearchRequestByUser();
+    this._favoriteMovies = favoriteMovies;
+  }
+
+  _listenToSearchRequestByUser() {
+    this._queryElement = document.getElementById('query');
+    this._queryElement.addEventListener('change', (event) => {
+      this._searchMovies(event.target.value);
+    });
+  }
+
+  _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery;
+    this._favoriteMovies.searchMovies(this.latestQuery);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _showFoundMovies(movies) {
+    const html = movies.reduce(
+      (carry, movie) => carry.concat(`
+        <li class="movie">
+            <span class="movie__title">${movie.title || '-'}</span>
+        </li>
+      `),
+      '',
+    );
+    document.querySelector('.movies').innerHTML = html;
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
+  }
+}
+
+export default FavoriteMovieSearchPresenter;
